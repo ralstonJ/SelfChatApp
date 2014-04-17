885fa5d415ca47206ed31edd6ea1a724aeddcbe6
Ralston J. Almeida <ralstonalmeida@gmail.com>
2014-04-17 00:28:54 -0400

    Added jade and other lib

--- server.js
+++ server.js
@@ -1,53 +1,147 @@
+/*var http = require('http');
+  http.createServer(function (req, res) {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Hello World\n');
+  }).listen(1337, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:1337/');
+*/
 
-/**
- * Module dependencies.
- */
+//Hell world
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
+/*
+var myModule = require("./hello.js");
+
+console.log("Hello: ",  myModule.hello());
+
+*/
+/*
 var http = require('http');
-var path = require('path');
-var socketio = require('socket.io');
+var fs = require('fs');
+console.log("Starting...");
+
+var config  = JSON.parse(fs.readFileSync('config.json'));
+
+var host = config.host;
+var port = config.port
+
+
+
+var server = http.createServer(function(request,response){
+	console.log("Received request from: " + request.url);
+	fs.readFile("./" + request.url,function(err,data){
+		if(err){
+			response.writeHead(404,{"Content-type":"text/plain"});
+			response.end("Sorry the page was not found")
+		}else{
+			response.writeHead(200,{"Content-type":"text/plain"});
+			response.write(data);
+			response.end();
+		}
+	});
+});
+
+server.listen(port,host,function(){
+	console.log("Listening : " + host + " : " + port );
+});
+
+fs.watchFile('config.json',function(){
+	config  = JSON.parse(fs.readFileSync('config.json'));
+	host = config.host;
+	port = config.port
+	server.close();
+	server.listen(port,host,function(){
+	console.log("Now Listening : " + host + " : " + port );
+});
+});
+
+*/
+
+/*
+
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync('config.json'));
+var host = config.host;
+var port = config.port;
+var express = require('express');
+
 var app = express();
 
+app.get('/',function(request,response){
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+	response.send("hello!");
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
- 
- 
-
-var server = http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
 });
 
 
-socketio.listen(server).on('connection', function (socket) {
-	
-	socket.on('message', function (msg) {
-		console.log('Message Received: ', msg);
+app.listen(port,host);
+
+*/
+/*
+var fs = require('fs');
+console.log("startimg..");
+fs.writeFile('./write_sync.txt','hello world',function(error){
+	console.log("written file");
+
+});
+
+*/
+
+/*
+var fs = require('fs');
+console.log("started..");
+var config = JSON.parse(fs.readFileSync('./config.json'));
+console.log("Initial config", config);
+
+fs.watchFile('./config.json',function(cur,prev){
+	console.log("config change");
+	config = JSON.parse(fs.readFileSync('./config.json'));
+	console.log("Now config is :", config);
+});
+
+*/
+/*
+var fs = require('fs');
+var mongo = require("mongodb");
+var config = JSON.parse(fs.readFileSync('./config.json'));
+var host = config.host;
+var port = mongo.Connection.DEFAULT_PORT;
+var db = new mongo.Db("
+", new mongo.Server(host,port,{}));
+db.open(function(error){
+	if(error){
 		
-		//Broadcast to all clients
-		socket.broadcast.emit('message', msg);
+		console.log(error);
+	
+	}else{
+	console.log("we are connected " + host + " : " + port );
+	
+	db.collection("user",function(error,collection){
+	if(error){
+		
+		console.log(error);
+	
+	}else{
+		
+		console.log("we have the collection");
+		
+		collection.insert({
+			name:"jarvis"
+		},function(){
+		
+			console.log("successfully Added");	
+		
+		});
+	}	
 	});
-
+	}
 });
-app.listen(process.env.port || 2455);
+
+*/
+
+var speech = require('google-speech-api');
+
+speech('/path/to/audio/file', function (err, results) {
+  console.log(results);
+  // [{status: 0, id: '...', hypotheses: [{utterance: 'this is a test', confidence: 0.9162679}]}]}]
+});
+
